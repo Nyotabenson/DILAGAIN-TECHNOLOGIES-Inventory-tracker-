fa1aefbe0b8d30c6222adb8f85aefd2d26a34356
Benson <nyota.benson752@gmail.com>
2024-05-10 12:23:48 +0300

    changes

--- app.py
+++ app.py
@@ -356,6 +356,6 @@ if total_sales:
     passcode3 = st.text_input("Passcode3")
     if passcode3 == '114986bn':
        total_sales = (m_tapes+m_btapes+m_a4+m_a5+m_ctns+m_ctnm+m_ctnl+m_pbm+m_pbl+m_s50)
-       st.write(total_sales)
+       st.write(total_sales-8500)
     elif passcode3 != '114986bn':
         st.markdown(":red[**Input Valid Passcode**]")    
